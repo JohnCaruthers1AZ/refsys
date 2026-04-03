efb1f34b1a49b12fa834e534018f5ddd15da59cd
JohnCaruthers1AZ <john.caruthers1@astrazeneca.com>
2026-04-03 16:59:34 -0400

    created import file function

--- rename_columns.py
+++ rename_columns.py
@@ -3,16 +3,11 @@ I want to create a file that inputs a dataframe and outputs a new
 dataframe with columns renamed
 """
 
-# Import packages
+# Import packages #
 import pandas as pd
 
-# Functions
-def rename_columns():
-    
-    #import data
-    temp_point_data = pd.read_csv(r"/home/engineering/Documents/PdM/data/temp_data/point_names.csv", low_memory=False)
-    temp_point_df = pd.DataFrame(temp_point_data)
-
+# Functions #
+def rename_columns_with_df():
     temp_data = pd.read_csv(r"/home/engineering/Documents/PdM/data/temp_data/temp_data_2026_01_19.csv", low_memory=False)
     temp_df = pd.DataFrame(temp_data)
 
@@ -30,3 +25,11 @@ def rename_columns():
         rename_dict[point_num[k]] = point_name[k]
 
     test_df.rename(columns=rename_dict)
+
+def import_files(path):
+    data = pd.read_csv(path, low_memory=False)
+    return pd.DataFrame(data)
+
+if __name__ == "__main__":
+    rename_columns_with_df()
+    import_files()
